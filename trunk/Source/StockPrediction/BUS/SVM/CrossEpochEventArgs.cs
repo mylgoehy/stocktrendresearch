a29a50916999e582dbd840333298df4718968b02
hotga2801 <hotga2801@3ea728c6-2ff9-0b1c-045f-72b5cbee0c70>
2011-03-24 04:31:43 +0000

    Hoàn tất chức năng progress bar cho cả ANN và SVM

---
+++ trunk/Source/StockPrediction/BUS/SVM/CrossEpochEventArgs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BUS.SVM
+{
+    /// <summary>
+    /// Cross Epoch Event Handler. This delegate handles events invoked whenever a training epoch
+    /// starts or ends
+    /// </summary>
+    /// <param name="sender">
+    /// The sender invoking the event
+    /// </param>
+    /// <param name="e">
+    /// Event Arguments
+    /// </param>
+    public delegate void CrossEpochEventHandler(object sender, CrossEpochEventArgs e);
+
+    /// <summary>
+    /// Cross Epoch Event Arguments
+    /// </summary>
+    public class CrossEpochEventArgs : EventArgs
+    {
+        private int _trainingIteration;
+        private int _cycles;
+
+        /// <summary>
+        /// Gets the current training iteration
+        /// </summary>
+        /// <value>
+        /// Current Training Iteration.
+        /// </value>
+        public int TrainingIteration
+        {
+            get { return _trainingIteration; }
+        }
+
+        /// <summary>
+        /// Get the number of cycles
+        /// </summary>
+        public int Cycles
+        {
+            get { return _cycles; }
+            set { _cycles = value; }
+        }
+
+        /// <summary>
+        /// Creates a new instance of training epoch event arguments
+        /// </summary>
+        /// <param name="trainingIteration">
+        /// Current training iteration
+        /// </param>
+        /// <param name="trainingSet">
+        /// The training set associated with the event
+        /// </param>
+        public CrossEpochEventArgs(int trainingIteration, int cycles)
+        {
+            this.Cycles = cycles;
+            this._trainingIteration = trainingIteration;
+        }
+    }
+}
