ed9bcb576b3237221fe2951c39f9e2206ba18b71
duyleminh1402@gmail.com <duyleminh1402@gmail.com@3ea728c6-2ff9-0b1c-045f-72b5cbee0c70>
2011-06-25 15:42:59 +0000

    sửa giao dien app, cap nhat update du lieu.

---
+++ trunk/Source/StockPrediction/BUS/UpdateDataBUS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace BUS
+{
+    public class UpdateDataBUS
+    {
+        private string _url = "http://www.cophieu68.com/export/excel.php?id=";
+        private string _uri;
+ 
+        public void DownloadStockData(string stockId, string savePath)
+        {
+            string _uri = _url + stockId;
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(_uri, savePath);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public void doUpdate(List<string> listStockIdUpdate, string updateFolder)
+        {            
+            for (int i = 0; i < listStockIdUpdate.Count; i++)
+            {
+                string stockId = listStockIdUpdate[i].ToUpper();
+                string fileName = updateFolder+stockId.ToLower()+".csv";                
+                DownloadStockData(stockId, fileName);
+            }
+        }
+    }
+}
